2082eb767d958f2a766774a0b812ad0371a2b952
tobinio <Tobias.frichmann1@gmail.com>
2024-10-23 06:43:05 +0000

    use dynamic ipAddr; update to uAgent 0.17.0

--- src/fetch_agent/fetch_agent/fetchAgent.py
+++ src/fetch_agent/fetch_agent/fetchAgent.py
@@ -1,11 +1,15 @@
 from uagents import Agent
 from uagents.setup import fund_agent_if_low
+import socket
+
+hostname = socket.gethostname()
+IPAddr = socket.gethostbyname(hostname)
 
 agent = Agent(
     name="car",
     seed="Car1",
     port=8002,
-    endpoint=["http://127.0.0.1:8002/submit"],
+    endpoint=["http://{}:8002/submit".format(IPAddr)],
 )
 
 fund_agent_if_low(agent.wallet.address())
