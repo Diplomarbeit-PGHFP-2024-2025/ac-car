a1ab323aee0e337dd9795606d0e1e9c9299cdee4
tobinio <Tobias.frichmann1@gmail.com>
2024-11-11 16:13:42 +0000

    feat: register at stations

--- src/fetch_agent/fetch_agent/communication.py
+++ src/fetch_agent/fetch_agent/communication.py
@@ -5,6 +5,11 @@ from aca_protocols.station_query_protocol import (
     StationQueryResponse,
 )
 
+from aca_protocols.car_register_protocol import (
+    CarRegisterRequest,
+    CarRegisterResponse
+)
+
 from aca_protocols.acs_registry_id import acs_id
 
 from .fetchAgent import agent
@@ -14,9 +19,24 @@ from .fetchAgent import agent
 async def on_is_registered(ctx: Context, sender: str, msg: StationQueryResponse):
     ctx.logger.info(f"stations: ${msg}")
 
+    for station in msg.stations:
+        await register_at_station(ctx, station)
+
 
 async def fetch_stations(ctx: Context):
     await ctx.send(
         acs_id,
         StationQueryRequest(lat=1.0, long=1.0, radius=5.0),
+    )
+
+
+@agent.on_message(CarRegisterResponse)
+async def on_registered_at_station(ctx: Context, sender: str, msg: CarRegisterResponse):
+    ctx.logger.info(f"registered state: ${msg}")
+
+
+async def register_at_station(ctx: Context, station: str):
+    await ctx.send(
+        station,
+        CarRegisterRequest(start_time=0, duration=10),
     )
