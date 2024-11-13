c69d2075ad23e87562517933dc40399ae7397475
leToxic <s.plasek41@gmail.com>
2024-11-13 10:02:10 +0000

    wip filtering

--- src/fetch_agent/fetch_agent/communication.py
+++ src/fetch_agent/fetch_agent/communication.py
@@ -1,6 +1,5 @@
 import asyncio
 import datetime
-from xmlrpc.client import DateTime
 
 from uagents import Context
 
@@ -11,7 +10,7 @@ from aca_protocols.station_query_protocol import (
 
 from aca_protocols.property_query_protocol import (
     PropertyQueryRequest,
-    PropertyQueryResponse
+    PropertyQueryResponse,
 )
 
 from aca_protocols.acs_registry_id import acs_id
@@ -21,31 +20,46 @@ from .fetchAgent import agent
 
 def init(ctx: Context):
     if ctx.storage.get("car_properties") is None:
-        ctx.storage.set("car_properties", PropertyQueryResponse(
-            open_time_frames=[
-                (datetime.datetime.now() + datetime.timedelta(hours=1)).timestamp(),
-                (datetime.datetime.now() + datetime.timedelta(hours=4)).timestamp(),
-            ],  # Default value: +- 4 hours from now is a possible Timeframe
-            geo_point=(1, 1),
-            cost_per_kwh=0.43,
-            charging_wattage=11,
-            green_energy=True
-        ))
+        ctx.storage.set(
+            "car_properties",
+            PropertyQueryResponse(
+                open_time_frames=[
+                    (
+                        (
+                            datetime.datetime.now() + datetime.timedelta(hours=1)
+                        ).timestamp(),
+                        (
+                            datetime.datetime.now() + datetime.timedelta(hours=4)
+                        ).timestamp(),
+                    ),
+                ],  # Default value: +1 and +4 hours from now is a possible Timeframe
+                geo_point=(1, 1),
+                cost_per_kwh=0.43,
+                charging_wattage=11,
+                green_energy=True,
+            ),
+        )
     if ctx.storage.get("station_to_property_map") is None:
         ctx.storage.set("station_to_property_map", {})
 
 
 @agent.on_message(StationQueryResponse)
-async def on_station_query_response(ctx: Context, sender: str, msg: StationQueryResponse):
+async def on_station_query_response(
+    ctx: Context, sender: str, msg: StationQueryResponse
+):
     ctx.logger.info(f"ACS-Registry ({sender}): stations: ${msg}")
 
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(build_station_map_and_send_property_requests(ctx, msg.stations))
+    loop.run_until_complete(
+        build_station_map_and_send_property_requests(ctx, msg.stations)
+    )
     loop.close()
 
 
 @agent.on_message(PropertyQueryResponse)
-async def on_property_query_response(ctx: Context, sender: str, msg: PropertyQueryResponse):
+async def on_property_query_response(
+    ctx: Context, sender: str, msg: PropertyQueryResponse
+):
     ctx.logger.info(f"AC-Station ({sender}): properties: ${msg}")
     station_to_property_map = ctx.storage.get("station_to_property_map")
     station_to_property_map[sender] = msg
@@ -65,32 +79,50 @@ async def fetch_stations(ctx: Context):
     )
 
 
-async def build_station_map_and_send_property_requests(ctx: Context, stations: list[str]):
+async def build_station_map_and_send_property_requests(
+    ctx: Context, stations: list[str]
+):
     ctx.logger.info("Starting building property map and requesting properties")
     station_to_property_map = {}
     for station in stations:
         station_to_property_map[station] = None
 
-        await ctx.send(
-            station,
-            PropertyQueryRequest()
-        )
+        await ctx.send(station, PropertyQueryRequest())
 
     ctx.storage.set("station_to_property_map", station_to_property_map)
 
 
-def filter_stations(ctx: Context, station_to_property_map: dict[str, PropertyQueryResponse]):
+def filter_stations(
+    ctx: Context, station_to_property_map: dict[str, PropertyQueryResponse]
+) -> dict[str, PropertyQueryResponse]:
     car_properties: PropertyQueryResponse = ctx.storage.get("car_properties")
-    station_to_property_map = sorted(station_to_property_map,
-                                     key=lambda item: sort_by_open_timeframe(car_properties.open_time_frames,
-                                                                             item.open_time_frames))
-
-    station_to_property_map = sorted(station_to_property_map,
-                                     key=lambda item: sort_by_distance(car_properties.geo_point,
-                                                                       item.geo_point))
+    sorted(
+        station_to_property_map,
+        key=lambda item: sort_by_all(car_properties, item[1]),
+    )
 
 
-def sort_by_open_timeframe(car_timeframe: list[(int, int)], station_timeframe: list[(int, int)]) -> int:
+def sort_by_all(
+    car_properties: PropertyQueryResponse, station_properties: PropertyQueryResponse
+) -> float:
+    return (
+        sort_by_open_timeframe(
+            car_properties.open_time_frames, station_properties.open_time_frames
+        )
+        + sort_by_distance(car_properties.geo_point, station_properties.geo_point)
+        + sort_by_cost(car_properties.cost_per_kwh, station_properties.cost_per_kwh)
+        + sort_by_charging_wattage(
+            car_properties.charging_wattage, station_properties.charging_wattage
+        )
+        + sort_by_green_energy(
+            car_properties.green_energy, station_properties.green_energy
+        )
+    ) / 5
+
+
+def sort_by_open_timeframe(
+    car_timeframe: list[(int, int)], station_timeframe: list[(int, int)]
+) -> int:
     # todo
     pass
 
@@ -104,14 +136,14 @@ def sort_by_cost(car_cost: float, station_cost: float) -> float:
     return abs(car_cost - station_cost)
 
 
-def sort_by_charging_wattage(car_cw: int, station_cw: int) -> int:
+def sort_by_charging_wattage(car_cw: int, station_cw: int) -> float:
     return abs(car_cw - station_cw)
 
 
 def sort_by_green_energy(car_ge: bool, station_ge: bool) -> int:
-    if car_ge == True and station_ge == True:
+    if car_ge is True and station_ge is True:
         return 1
-    elif car_ge == True and station_ge == False:
+    elif car_ge is True and station_ge is False:
         return -1
     else:
         return 0
